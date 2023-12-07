ce705008d836676dd28187cd3e732f0f5e3fa67e
unknown <itayadgarov@gmail.com>
2023-12-07 22:29:42 +0200

    now you need to do the rout that brings the chats by the date updatd

--- server/routs/chats.js
+++ server/routs/chats.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const {chatModel, validateMessage}= require('../models/chatModel'); 
 const {auth}=require("../midlewares/auth")
  
-router.get('/showChat/:id', auth, async(req, res)=>{
+router.get('/showChat/:userId', auth, async(req, res)=>{
     try{
         const id1=req.tokenData._id;
-        const id2 = req.params.id;
-        const chat = chatModel.find({$or:[{id1, id2}, {id1:id2, id2:id1}]});
+        const id2 = req.params.userId;
+        const chat = await chatModel.findOne({$or:[{id1, id2}, {id1:id2, id2:id1}]});
+
         if (!chat) {
             return res.status(404).json({ message: 'Chat not found.' });
+        }
+   
+        if(chat.id1!=id1&&chat.id2!=id1){
+            return res.status(401).json({message: "you are not a participant of this chat"})
         }
         res.status(201).json(chat);
     }catch(error){
@@ -26,7 +31,6 @@ router.post('/createChat/:id',auth, async (req, res) => {
     if (existingChat) {
       return res.status(400).json({ message: 'Chat already exists.' });
     }
-
     const newChat = new chatModel({ id1, id2 });
 
     await newChat.save();
@@ -53,7 +57,10 @@ router.put('/sendMessage/:chatId',auth, async (req, res) => {
     if (!chat) {
       return res.status(404).json({ message: 'Chat not found.' });
     }
-
+    const id = req.tokenData._id;
+    if(chat.id1!=id&&chat.id2!=id){
+      return res.status(401).json({message: "you are not a participant of this chat"})
+    }
     chat.messages.push({ text, senderId });
 
     await chat.save();
